33151dc0054064d522de750d3c8763e4ad3b9586
ZerΔch <zerach.sec@gmail.com>
2025-09-06 14:18:51 +0530

    Create SkillCoin.sol

---
+++ SkillCoin.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SkillCoin {
+    string public name;
+    string public symbol;
+    uint256 decimals;
+    uint256 totalSupply;
+
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    // event approve(address indexed from, address indexed _spender, uint256 _amount);
+
+    constructor (){
+        name = "SkillCoin";
+        symbol = "SK";
+        decimals = 18;
+    }
+
+    function balanceOf(address account) public view returns(uint256) {
+        return balances[account];
+    }
+
+    function transfer (address _to, uint256 _amount) public returns (bool) {
+        require (balanceOf(msg.sender) > _amount, "not enough eth");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approve (address _spender,uint256 _amount) public returns (bool){
+        allowances[msg.sender][_spender] = _amount;
+        //emit approve(msg.sender,_spender, _amount);
+        return true;
+    } 
+
+    function allowance(address owner, address _spender) public view returns(uint256){
+        return allowances[owner][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool){
+        require(allowance(_from, _to) >= _value, "not enough allowance");
+        require(balances[_from] >= _value,"not enough balance");
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][_to] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function mint (uint256 amount) public {
+        totalSupply += amount;
+        balances[msg.sender] += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+}
